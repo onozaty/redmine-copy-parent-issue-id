993a54b98bbe468af77041349fe3874508f88916
onozaty <onozaty@gmail.com>
2015-07-15 07:32:08 +0000

    Changed issue_patch.rb path.

--- init.rb
+++ init.rb
@@ -1,10 +1,10 @@
-require_dependency 'issue_patch'
+require 'copy_parent_issue_id/issue_patch'
 
 Redmine::Plugin.register :copy_parent_issue_id do
   name 'Copy parent issue id plugin'
   author 'onozaty'
   description 'Copy parent issue id plugin for Redmine'
-  version '1.0.0'
+  version '1.0.1'
   url 'https://github.com/onozaty/redmine-copy-parent-issue-id'
   author_url 'https://github.com/onozaty'
 end
